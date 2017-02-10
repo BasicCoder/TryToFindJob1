930707d9a5b7a69525e07f0e40cea55c9a9b8eb1
BasicCoder <abasiccoder@gmail.com>
2017-02-10 10:33:00 +0800

    add a problem code

---
+++ LetCode21.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* p = head;
+
+        while(l1 != NULL || l2 != NULL){
+            ListNode* cur = NULL;
+            if(l1 == NULL){
+                cur = l2;
+                l2 = l2 -> next;
+            }else if(l2 == NULL){
+                cur = l1;
+                l1 = l1 -> next;
+            }else{
+                if(l1 -> val < l2 -> val){
+                    cur = l1;
+                    l1 = l1 -> next;
+                }else{
+                    cur = l2;
+                    l2 = l2 -> next;
+                }
+            }
+            p -> next = cur;
+            p = p -> next;
+        }
+
+        return head -> next;
+    }
+};
+
+int main(){
+
+}
